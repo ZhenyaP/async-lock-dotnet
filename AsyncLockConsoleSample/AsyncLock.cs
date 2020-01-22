1962de955e5471203156beaa523c8e8167281a22
Eugene Petrovich <zhpetrovich@yahoo.com>
2020-01-22 20:30:34 +0300

    Added the AsyncLockConsoleSample solution

---
+++ AsyncLockConsoleSample/AsyncLock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncLockConsoleSample
+{
+    public class AsyncLock
+    {
+        private Task _unlockedTask = Task.CompletedTask;
+
+        public async Task<Action> Lock()
+        {
+            var tcs = new TaskCompletionSource<object>();
+
+            await Interlocked.Exchange(ref _unlockedTask, tcs.Task);
+
+            return () => tcs.SetResult(null);
+        }
+    }
+}
---
+++ AsyncLockConsoleSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncLockConsoleSample
+{
+    class Program
+    {
+        private static async Task Foo()
+        {
+            var _lock = new AsyncLock();
+            var unlock = await _lock.Lock();
+            unlock();         //if we comment out this line, we will have a deadlock,
+                              // as the lock has already been acquired !
+            for (var i = 0; i < 10; i++) await Bar(_lock);
+
+            //unlock();
+        }
+
+        private static async Task Bar(AsyncLock _lock)
+        {
+            var unlock = await _lock.Lock();
+            // do something sync
+            unlock();
+        }
+
+        static async Task Main(string[] args)
+        {
+            await Foo();
+        }
+    }
+}
